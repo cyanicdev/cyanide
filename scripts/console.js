d79b7440d197eb58b96da0d851ec87249258b687
Javad Hamidi <JahmHV@gmail.com>
2019-07-04 20:06:01 +0930

    Bug fixing

--- scripts/console.js
+++ scripts/console.js
@@ -10,22 +10,34 @@
     var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
 
     function init() {
-
+        /*
+        let overtype = true;
+        function overtypeBlink() {
+            if (overtype) { output.innerText = consoleText + '\u2583'; }
+            else { output.innerText = consoleText }
+            overtype = !overtype;
+        }
+        setInterval(overtypeBlink, 500);
+        */
         let output = document.getElementById('output');
 
         function runCommand(command) {
           exec(command, (err, stdout, stderr) => {
-            if (err) {
+            /* if (err) {
               // node couldn't execute the command
               consoleText += '\n' + err;
-          }
+            } */
 
             // the *entire* stdout and stderr (buffered)
-            consoleText +=  `\n stdout: ${stdout}`;
-            consoleText +=  `\n stderr: ${stderr}`;
+            if (stdout) {
+                consoleText +=  `\n ${stdout}`;
+            } else {
+              consoleText +=  `\n ${stderr}`;
+            }
             consoleText += '\n'
 
-            output.innerText = consoleText;
+            output.innerText = consoleText + '\u2583';
+            document.getElementById('console-screen').scrollTo(0,document.getElementById('console-screen').scrollHeight);
           });
         }
 
@@ -35,10 +47,15 @@
             //let keynum;
             // if (window.event) { keynum = e.key; }
             if (e.key.length == 1) { consoleText += e.key; }
-            else if (e.key == 'Backspace') { consoleText = consoleText.slice(0, -1); }
+            else if (e.key == 'Backspace') {
+              if (consoleText.slice(-1) != '\n') {
+                  consoleText = consoleText.slice(0, -1);
+              }
+            }
             else if (e.key == 'Enter') { runCommand(consoleText.split("\n")[consoleText.split("\n").length-1]); }
 
-            output.innerText = consoleText;
+            output.innerText = consoleText + '\u2583';
+            document.getElementById('console-screen').scrollTo(0,document.getElementById('console-screen').scrollHeight);
         }, false);
 
 
